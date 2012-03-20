69ffd9151aa02755161121a4a3ddc883f7398164
SkidzTweak <skidz.tweak@gmail.com>
2012-03-19 20:11:38 -0700

    Fixing small mistake

--- Addon-Modules/AuroraWebUI/Modules/WebUIHandler.cs
+++ Addon-Modules/AuroraWebUI/Modules/WebUIHandler.cs
@@ -751,6 +751,7 @@ namespace Aurora.Services
                 resp["UUID"] = OSD.FromUUID(account.PrincipalID);
                 resp["FirstName"] = OSD.FromString(account.FirstName);
                 resp["LastName"] = OSD.FromString(account.LastName);
+				resp["Email"] = OSD.FromString(account.Email);
             }
 
             resp["Verified"] = OSD.FromBoolean(Verified);
