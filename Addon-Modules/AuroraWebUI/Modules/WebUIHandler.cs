3b9d32c399dc7dcb2c0691a78a81f874336e0156
Skidz Tweak <skidz.tweak@gmail.com>
2012-04-07 17:17:31 -0300

    Update Addon-Modules/AuroraWebUI/Modules/WebUIHandler.cs

--- Addon-Modules/AuroraWebUI/Modules/WebUIHandler.cs
+++ Addon-Modules/AuroraWebUI/Modules/WebUIHandler.cs
@@ -752,6 +752,7 @@ namespace Aurora.Services
                 resp["FirstName"] = OSD.FromString(account.FirstName);
                 resp["LastName"] = OSD.FromString(account.LastName);
 				resp["Email"] = OSD.FromString(account.Email);
+				Verified - true;
             }
 
             resp["Verified"] = OSD.FromBoolean(Verified);
